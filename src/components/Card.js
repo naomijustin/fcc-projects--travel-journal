e67ecf8aedf05aaca8ee4413cd6605c55cd1d55e
naomijustin <naomi.z.justin@gmail.com>
2023-07-17 08:04:25 +1000

    Further styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,13 +9,12 @@ export default function Card (props) {
                 <div className="card-title-row">
                     <img src={process.env.PUBLIC_URL + '/images/pin-icon.svg'} alt="Pin Icon" type="images/svg+xml" className="icon-pin"></img>
                     <p>{props.item.location.toUpperCase()}</p>
-                    <a src={props.item.mapLink}>View on Google Maps</a>
+                    <a src={props.item.mapLink} className="card-maps-link">View on Google Maps</a>
                 </div>                
-                <h2>{props.item.title}</h2>
+                <h2 className="card-h2-title">{props.item.title}</h2>
                 <span>{props.item.startDate} - {props.item.endDate}</span>
                 <p>{props.item.description}</p>
-            </div>
-            
+            </div>            
         </section>
     )
 }
